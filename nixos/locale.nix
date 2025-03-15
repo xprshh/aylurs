3815073c28f518b23fa39fde6ac2bffd0bffe6ea
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-16 00:53:28 +0100

    Update locale.nix

--- nixos/locale.nix
+++ nixos/locale.nix
@@ -1,17 +1,17 @@
 {
-  time.timeZone = "America/New_York";  // Change timezone to New York
+  time.timeZone = "America/New_York";  
 
-  i18n.defaultLocale = "en_US.UTF-8";  // Set default language to English (US)
+  i18n.defaultLocale = "en_US.UTF-8"; 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "hu_HU.UTF-8";         // Keep Hungarian locale for addresses
-    LC_IDENTIFICATION = "hu_HU.UTF-8";  // Keep Hungarian locale for identification
-    LC_MEASUREMENT = "hu_HU.UTF-8";     // Keep Hungarian locale for measurement
-    LC_MONETARY = "hu_HU.UTF-8";        // Keep Hungarian locale for monetary values
-    LC_NAME = "hu_HU.UTF-8";            // Keep Hungarian locale for names
-    LC_NUMERIC = "hu_HU.UTF-8";         // Keep Hungarian locale for numeric formatting
-    LC_PAPER = "hu_HU.UTF-8";           // Keep Hungarian locale for paper-related settings
-    LC_TELEPHONE = "hu_HU.UTF-8";       // Keep Hungarian locale for telephone formats
-    LC_TIME = "hu_HU.UTF-8";            // Keep Hungarian locale for time settings
+    LC_ADDRESS = "hu_HU.UTF-8";         
+    LC_IDENTIFICATION = "hu_HU.UTF-8";  
+    LC_MEASUREMENT = "hu_HU.UTF-8";    
+    LC_MONETARY = "hu_HU.UTF-8";       
+    LC_NAME = "hu_HU.UTF-8";          
+    LC_NUMERIC = "hu_HU.UTF-8";         
+    LC_PAPER = "hu_HU.UTF-8";           
+    LC_TELEPHONE = "hu_HU.UTF-8";      
+    LC_TIME = "hu_HU.UTF-8";  
   };
 
   console.useXkbConfig = true;
