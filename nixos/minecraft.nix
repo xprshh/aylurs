1692de67ef9321b4d4fb8bfff9eff73f719e22c9
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-19 22:46:23 -0400

    Update minecraft.nix

--- nixos/minecraft.nix
+++ nixos/minecraft.nix
@@ -1,6 +1,15 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 
 {
+  hardware.xpadneo.enable = true; # Enable the xpadneo driver for Xbox One wireless controllers
+
+  boot = {
+    extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
+    extraModprobeConfig = ''
+      options bluetooth disable_ertm=Y
+    '';
+  };
+
   environment.systemPackages = with pkgs; [
     lunar-client
   ];
