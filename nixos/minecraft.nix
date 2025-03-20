fcdcbe095ef17f60ed12dc96ff73c2b122da184a
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-19 22:29:19 -0400

    Create minecraft.nix idk

---
+++ nixos/minecraft.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, ...}: 
+
+{
+  services.minecraft-server = {
+    enable = true;
+    eula = true;
+    declarative = true;
+
+    package = pkgs.minecraft-server-1-12;
+    dataDir = "/var/lib/minecraft";
+
+    serverProperties = {
+      gamemode = "survival";
+      difficulty = "hard";
+      simulation-distance = 10;
+      level-seed = "4";
+    };
+
+    whitelist = {
+      vimjoyer = "3f96da12-a55c-4871-8e26-09256adac319";
+    };
+
+    jvmOpts = "-Xms4092M -Xmx4092M -XX:+UseG1GC";
+  };
+}
