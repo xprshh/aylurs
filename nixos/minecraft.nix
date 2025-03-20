29fbb7bada81eb10f3df59f0dadbed6375e96d7a
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-19 22:43:36 -0400

    Update minecraft.nix

--- nixos/minecraft.nix
+++ nixos/minecraft.nix
@@ -1,25 +1,7 @@
-{ pkgs, lib, ...}: 
+{ pkgs, lib, ... }:
 
 {
-  services.minecraft-server = {
-    enable = true;
-    eula = true;
-    declarative = true;
-
-    package = pkgs.minecraft-server;
-    dataDir = "/var/lib/minecraft";
-
-    serverProperties = {
-      gamemode = "survival";
-      difficulty = "hard";
-      simulation-distance = 10;
-      level-seed = "4";
-    };
-
-    whitelist = {
-      vimjoyer = "3f96da12-a55c-4871-8e26-09256adac319";
-    };
-
-    jvmOpts = "-Xms4092M -Xmx4092M -XX:+UseG1GC";
-  };
+  environment.systemPackages = with pkgs; [
+    lunar-client
+  ];
 }
