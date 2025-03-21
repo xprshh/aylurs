bacabe71bc9477d381ee2073c793c4971bffb210
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-21 12:20:07 -0400

    Update vulkan.nix

--- nixos/vulkan.nix
+++ nixos/vulkan.nix
@@ -4,13 +4,11 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    vulkanSupport = true; 
-    intel = {
-      enable = true;
-      forceProbe = false; # Set to true if using an unsupported GPU
-    };
+    vulkanSupport = true; # Enable Vulkan support
   };
+
   programs.gamescope.enable = true;
+
   services.xserver.videoDrivers = ["modesetting"];
 
   services.xserver.deviceSection = ''
