95431f0f37e6aa04c084df18200570e49bccf5cb
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-21 12:16:41 -0400

    Update vulkan.nix

--- nixos/vulkan.nix
+++ nixos/vulkan.nix
@@ -10,7 +10,7 @@
       forceProbe = false; # Set to true if using an unsupported GPU
     };
   };
-  gamescope.enable = true;
+  programs.gamescope.enable = true;
   services.xserver.videoDrivers = ["modesetting"];
 
   services.xserver.deviceSection = ''
