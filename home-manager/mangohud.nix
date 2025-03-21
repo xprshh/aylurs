76c2333d245611d1270749a379af51cb3da3d2c1
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-21 12:15:19 -0400

    Create mangohud.nix

---
+++ home-manager/mangohud.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }: {
+  programs.mangohud = {
+    enable = true;
+    settings = {
+      preset = "-1";
+
+      font_file = "${pkgs.iosevka}/share/fonts/truetype/Iosevka-Bold.ttf";
+      font_size = 24;
+      round_corners = 11.0;
+
+      gpu_temp = true;
+      cpu_temp = true;
+
+      ram = true;
+      vram = true;
+
+      engine_version = true;
+    };
+  };
+}
