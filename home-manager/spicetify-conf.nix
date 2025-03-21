248d82dc748723046a577f8131e617f4e9761bfd
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-20 23:29:15 -0400

    Create spicetify-conf.nix

---
+++ home-manager/spicetify-conf.nix
@@ -0,0 +1,23 @@
+{ pkgs, inputs, ... }:
+
+programs.spicetify =
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+in
+{
+  enable = true;
+
+  enabledExtensions = with spicePkgs.extensions; [
+    adblock
+    hidePodcasts
+    shuffle # shuffle+ (special characters are sanitized out of extension names)
+  ];
+  enabledCustomApps = with spicePkgs.apps; [
+    newReleases
+    ncsVisualizer
+  ];
+  enabledSnippets = with spicePkgs.snippets; [
+    rotatingCoverart
+    pointer
+  ];  
+}
