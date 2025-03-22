cbea2f21e840c7b82fc2ae46d3815f53d95f85ed
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-22 10:05:29 -0400

    Create boot.nix

---
+++ nixos/boot.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  boot = {
+    bootspec.enable = true;
+
+    initrd = {
+      systemd.enable = true;
+      supportedFilesystems = ["ntfs"];
+    };
+
+    # use latest kernel
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    consoleLogLevel = 3;
+    kernelParams = [
+      "quiet"
+      "systemd.show_status=auto"
+      "rd.udev.log_level=3"
+      "plymouth.use-simpledrm"
+    ];
+
+    loader = {
+      # systemd-boot on UEFI
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    plymouth.enable = true;
+
+    tmp.cleanOnBoot = true;
+  };
+  environment.systemPackages = [config.boot.kernelPackages.cpupower];
+}
