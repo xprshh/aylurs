7083f167392c6e146c4ef5fa2d546e3eec2ea213
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-22 17:44:03 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
       url = "github:nix-community/home-manager";
